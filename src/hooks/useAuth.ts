ad84b5913870e3cc87f01fc6ac03d9b855260418
jhlee <wisdom_13_@naver.com>
2024-06-23 02:28:10 +0900

    ✨ 로그인 여부 체크

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import { User } from 'firebase/auth';
+import { auth } from '@/config/firebase';
+
+const useAuth = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return { user, loading };
+};
+
+export default useAuth;
--- src/pages/Main/Home.tsx
+++ src/pages/Main/Home.tsx
@@ -1,14 +1,33 @@
-import { app } from '@/config/firebase';
+import { signOut } from 'firebase/auth';
+import { auth } from '@/config/firebase';
+import useAuth from '@/hooks/useAuth';
 
-
-console.log("app", app);
+import { Spinner } from '@/components/Spinner';
+import { Button } from '@/components/ui/button';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
+  const { user, loading } = useAuth();
+
   return (
     <>
       <div>home</div>
+      {loading && <Spinner />}
+      {
+        user ? (
+          <>
+            <div>
+              {user?.displayName}님 환영합니다.
+            </div>
+            <Button onClick={() => signOut(auth)}>로그아웃</Button>
+          </>
+        ) : (
+          <Button>
+            <Link to='/auth/login'>로그인</Link>
+          </Button>
+        )
+      }
 
-      <div className='test'></div>
     </>
   );
 }
