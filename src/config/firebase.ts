931cc7645cd332602a9484fb2091cab87fe88f5f
jhlee <wisdom_13_@naver.com>
2024-06-22 11:13:26 +0900

    ⚙️ firebase 설정

---
+++ src/config/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app
