f615edf20ad0bbd79c4a3cbba6ceb40e823704ca
jhlee <wisdom_13_@naver.com>
2024-07-03 01:16:47 +0900

    🐎 데이터 PreFethcing 적용

--- src/components/Main/CategoryItem.tsx
+++ src/components/Main/CategoryItem.tsx
@@ -4,17 +4,16 @@ import { Link } from 'react-router-dom';
 
 interface CategoryItemProps {
   category?: string;
-  link: string;
   text: string;
   size?: 'sm' | 'md';
 }
 
-const CategoryItem = ({ category = '', link, text, size = 'sm' }: CategoryItemProps) => {
+const CategoryItem = ({ category = '', text, size = 'sm' }: CategoryItemProps) => {
   const filters = { categories: [category], 'productsState': true, 'sortBy': 'byDate' }
   const { prefetchProducts } = usePrefetchProducts(filters);
 
   return (
-    <Link to={link} onMouseEnter={prefetchProducts}>
+    <Link to={category ? `/category/${category}` : '/vitamins'} onMouseEnter={prefetchProducts}>
       <div className={cn(
         'flex justify-center items-center gap-y-2 bg-gray-100 hover:bg-primary rounded-md w-full h-16 hover:text-white transition-all cursor-pointer',
         `text-${size}`
